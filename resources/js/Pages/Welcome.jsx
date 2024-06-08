d139708f361fc993f4c1cdb220458f04763d0bcd
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-06-08 21:12:54 +0100

    Banner added to home page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,20 +1,20 @@
 import Navbar from "@/Components/Navbar";
 import Slide from "@/Components/Slide";
 import Services from "@/Components/Services";
+import Banner from "@/Components/Banner";
 import { Link, Head } from "@inertiajs/react";
 
 export default function Welcome({ categories, bestproducts }) {
-    // { auth, laravelVersion, phpVersion }
-    // const handleImageError = () => {
-    //     document
-    //         .getElementById("screenshot-container")
-    //         ?.classList.add("!hidden");
-    //     document.getElementById("docs-card")?.classList.add("!row-span-1");
-    //     document
-    //         .getElementById("docs-card-content")
-    //         ?.classList.add("!flex-row");
-    //     document.getElementById("background")?.classList.add("!hidden");
-    // };
+    const BannerData = {
+        discount: "30% OFF",
+        title: "Fine Smile",
+        date: "10 Jan to 28 Jan",
+        image: "http://127.0.0.1:8000/storage/products/headphone.png",
+        title2: "Air Solo Bass",
+        title3: "Winter Sale",
+        title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis",
+        bgColor: "#f42c37",
+    };
 
     return (
         <>
@@ -22,6 +22,7 @@ export default function Welcome({ categories, bestproducts }) {
             <Navbar categories={categories} />
             <Slide bestproducts={bestproducts} />
             <Services />
+            <Banner data={BannerData} />
         </>
     );
 }
