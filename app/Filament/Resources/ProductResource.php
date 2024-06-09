6ba648b4d0f16968706aad0725a226676a057e5f
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-06-09 14:32:54 +0100

    custumize admin menu

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -18,7 +18,11 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-archive-box';
+
+    protected static ?string $navigationGroup = 'Store Management';
+
+    protected static ?int $navigationSort = 1;
 
     public static function form(Form $form): Form
     {
