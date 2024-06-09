aa1018ca97e01d4ec30108caacbbd259eae27a09
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-06-09 15:04:52 +0100

    add orders widget to dashboard , new package used , Trend

---
+++ app/Filament/Widgets/OrdersChart.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class OrdersChart extends ChartWidget
+{
+    protected static ?string $heading = 'Orders Chart';
+
+    protected static string $color = 'primary';
+
+    protected static ?string $pollingInterval = '60s';
+
+    protected function getData(): array
+    {
+        $data = Trend::query(
+            Order::query()
+        )
+            ->between(
+                start: now()->subMonths(12),
+                end: now(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Number of orders',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            'scales' => [
+                'y' => [
+                    'beginAtZero' => true,
+                    'ticks' => [
+                        'stepSize' => 1, // Display only whole numbers
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
