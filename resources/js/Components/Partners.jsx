b4a18e2a5adc7a8ef9675fb15860f4939d2751dc
SEBAA-Mohammed <mohammedsebaa2610@gmail.com>
2024-06-10 19:20:58 +0100

    some styme changes

--- resources/js/Components/Partners.jsx
+++ resources/js/Components/Partners.jsx
@@ -1,9 +1,6 @@
 const Partners = () => {
     return (
-        <div
-            data-aos="zoom-out"
-            className="py-8 mt-24 hidden md:block bg-gray-200 dark:bg-white/10"
-        >
+        <div className="py-8 mt-24 hidden md:block bg-gray-200 dark:bg-white/10">
             <div className="container">
                 <div className="grid grid-cols-5 gap-3 place-items-center opacity-50">
                     <img
